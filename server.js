6685a7d91203eb0bdd674a533a649cd374664b97
Raksit Mantanacharu <raksit.m@ku.th>
2018-04-29 17:06:20 +0700

    fixed path resolve

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.static(__dirname + '/build'))
 
 app.get('*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, 'index.html'))
+    res.sendFile(path.resolve(__dirname + '/build', 'index.html'))
 })
 
 app.listen(port)
